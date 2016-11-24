85da4e6017ad5516e543c1cbd349b83054bc2f4d
vlad <vladkuraev84@gmail.com>
2016-11-24 12:23:12 +0200

    merg 24.11

--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,33 @@
       }
     });
 // };
+/* ===========header============*/ 
+  $(window).scroll(function(){
+        var bo = $("body").scrollTop();
+        if ( bo > 750 ) {$(".burger").css("display", "block"); } 
+        else { 
+        	$(".burger").css("display", "none"); 
+        	$('.burger__item').removeClass('burger__item--active');
+        	$(".mob-nav").slideUp(700);
+      	};
+  });
+
+/* ===========burger============*/ 
+  $('.burger__item').click(function(){
+  	$('.burger__item').toggleClass('burger__item--active');
+		$(".mob-nav").slideToggle(700);			
+	});
+	$('main').click(function(){
+		  $('.burger__item').removeClass('burger__item--active');
+			$(".mob-nav").slideUp(300);	
+		});
+
+	$('.mob-nav__link--first').click(function(){
+			$('.mob-nav__body').hide();
+			$(".serv-menu").show();			
+	});
+
+	$('.serv-menu__title a').click(function(){
+			$(".serv-menu").hide();			
+			$('.mob-nav__body').show();
+	});
