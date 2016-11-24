db9b328ea721d6cbfc8bb4221b7c315e61b7c929
vlad <vladkuraev84@gmail.com>
2016-11-24 11:26:08 +0200

    com 24.11

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,18 @@
 ﻿  $('.bxslider').bxSlider({
     pagerCustom: '#bx-pager'
   });
+
 // $(document).ready(function(){
-// }
+	$('.arrow-our-serv').click(function () {
+		$('.our-serv').slideToggle('slow');
+	});
+	$('.our-serv-close').click(function () {
+		$('.our-serv').slideToggle('slow');
+	});
+	 $(document).mouseup(function (e) {
+      var container = $(".our-serv");
+      if (container.has(e.target).length === 0){
+          container.fadeOut();
+      }
+    });
+// };
