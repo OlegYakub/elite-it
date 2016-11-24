48df1d2c175d297f7ff46a35f87b5729071b58d5
OlegYakub <zeiro299@mail.ru>
2016-11-24 11:22:43 +0200

    Добавил хедер

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,34 @@
 ﻿  $('.bxslider').bxSlider({
     pagerCustom: '#bx-pager'
   });
-// $(document).ready(function(){
-// }
+
+/* ===========header============*/ 
+  $(window).scroll(function(){
+        var bo = $("body").scrollTop();
+        if ( bo > 500 ) {$(".burger").css("display", "block"); } 
+        else { 
+        	$(".burger").css("display", "none"); 
+        	$('.burger__item').removeClass('burger__item--active');
+        	$(".mob-nav").slideUp(700);
+      	};
+  });
+
+/* ===========burger============*/ 
+  $('.burger__item').click(function(){
+  	$('.burger__item').toggleClass('burger__item--active');
+		$(".mob-nav").slideToggle(700);			
+	});
+	$('main').click(function(){
+		  $('.burger__item').removeClass('burger__item--active');
+			$(".mob-nav").slideUp(300);	
+		});
+
+	$('.mob-nav__link--first').click(function(){
+			$('.mob-nav__body').hide();
+			$(".serv-menu").show();			
+	});
+
+	$('.serv-menu__title a').click(function(){
+			$(".serv-menu").hide();			
+			$('.mob-nav__body').show();
+	});
